419ac7c539da54e555651acd6ae76849cd18e8f4
Harry Kimpel <harrykimpel@hotmail.com>
2026-06-05 13:14:21 +0200

    Minor update to Home.razor, fixing docker-entrypoint.sh to correctly refer to the profiler

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,7 +2,7 @@
 set -e
 
 case "$(uname -m)" in
-  x86_64)  export CORECLR_PROFILER_PATH=/app/newrelic/linux-x64/libNewRelicProfiler.so ;;
+  x86_64)  export CORECLR_PROFILER_PATH=/app/newrelic/libNewRelicProfiler.so ;;
   aarch64) export CORECLR_PROFILER_PATH=/app/newrelic/linux-arm64/libNewRelicProfiler.so ;;
   *)       echo "Unsupported architecture: $(uname -m)" >&2; exit 1 ;;
 esac
